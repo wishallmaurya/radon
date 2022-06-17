aba8e44ea1032857086f467b9f66f3d2e57c4b05
wishallmaurya <vm7740014@gmail.com>
2022-06-17 15:29:34 +0530

    error-handale

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://wishall:vishal@atlascluster.p9u9uvd.mongodb.net/vishal13-db?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
@@ -19,4 +19,4 @@ app.use('/', route)
 
 app.listen(process.env.PORT || 3000, function () {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
-});
+}); 
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,12 +1,46 @@
-const authenticate = function(req, req, next) {
-    //check the token in request header
-    //validate this token
+const userModel = require("../models/userModel");
+const jwt = require("jsonwebtoken");
 
-    next()
+
+//check the token in request header
+//validate this token
+const authenticate = function(req, res, next) {
+    let token = req.headers["x-Auth-token"];
+    if (!token) token = req.headers["x-auth-token"];
+    if (!token) return res.send({ status: false, msg: "token must be present" });
+    
+    let decodedToken = jwt.verify(token, "functionup-thorium");
+    if (!decodedToken ) {
+      return res.send({ status: false, msg: "token is invalid" });
+    }
+    next();
 }
 
 
-const authorise = function(req, res, next) {
-    // comapre the logged in user's id and the id in request
-    next()
+
+
+
+const authorise = async function(req, res, next) {
+// comapre the logged in user's id and the id in request
+let userId = req.params.userId;
+let userDetails = await userModel.findById(userId);
+if (!userDetails)
+  return res.send({ status: false, msg: "No such user exists" });
+  let token = req.headers["x-auth-token"]
+  if (!token) return res.send({ status: false, msg: "token must be present in the request header" })
+  let decodedToken = jwt.verify(token, 'functionup-thorium')
+
+  if (!decodedToken) return res.send({ status: false, msg: "token is not valid" })
+
+  
+  let userToBeModified = req.params.userId;
+
+  let userLoggedIn = decodedToken.userId
+
+ 
+  if (userToBeModified != userLoggedIn) return res.send({ status: false, msg: 'User logged is not allowed to modify the requested users data' })
+
+next()
 }
+module.exports.authenticate= authenticate
+module.exports.authorise= authorise
