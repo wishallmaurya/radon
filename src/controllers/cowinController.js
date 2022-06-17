f6f5f2a8ec1aef5a627ad1ae4f6714d8aeddbbe3
wishallmaurya <vm7740014@gmail.com>
2022-06-17 17:39:06 +0530

    done

--- src/controllers/cowinController.js
+++ src/controllers/cowinController.js
@@ -28,7 +28,23 @@ let getDistricts = async function (req, res) {
             url: `https://cdn-api.co-vin.in/api/v2/admin/location/districts/${id}`
         }
         let result = await axios(options);
-        console.log(result)
+        let data = result.data
+        res.status(200).send({ msg: data, status: true })
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).send({ msg: err.message })
+    }
+}
+let centresindistrict = async function (req, res) {
+    try {
+        let district_id = req.query.district_id
+        let date = req.query.date
+            let options = {
+            method: "get",
+            url: `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=${district_id}&date=${date}`
+        }
+        let result = await axios(options);
         let data = result.data
         res.status(200).send({ msg: data, status: true })
     }
@@ -83,3 +99,4 @@ module.exports.getStates = getStates
 module.exports.getDistricts = getDistricts
 module.exports.getByPin = getByPin
 module.exports.getOtp = getOtp
+module.exports.centresindistrict = centresindistrict
--- src/routes/route.js
+++ src/routes/route.js
@@ -10,7 +10,11 @@ router.get("/test-me", function (req, res) {
 
 
 router.get("/cowin/states", CowinController.getStates)
+
 router.get("/cowin/districtsInState/:stateId", CowinController.getDistricts)
+
+router.get("/cowin/centresindistrict", CowinController.centresindistrict)
+
 router.get("/cowin/getByPin", CowinController.getByPin)
 
 router.post("/cowin/getOtp", CowinController.getOtp)
